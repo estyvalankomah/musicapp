274f0991154daa85ffa782b99cb30e1829fc70e3
Esther Ankomah <ns13030132@gmail.com>
2022-07-28 22:11:41 +0000

    first commit

---
+++ src/components/CenterMenu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function CenterMenu() {
+  return (
+    <div className="menu flex">
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Preformer</li>
+            <li>Event Schedule</li>
+        </ul>
+    </div>
+  )
+}
+
+export default CenterMenu
