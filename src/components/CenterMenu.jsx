bea9dae4d1f917b563acbdc4c005275440c9fdcf
Esther Ankomah <ns13030132@gmail.com>
2022-08-04 07:46:23 +0000

    done

--- src/components/CenterMenu.jsx
+++ src/components/CenterMenu.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 function CenterMenu() {
+    const liStyle = "mr-[3rem] hover:cursor-pointer"
   return (
     <div className="menu flex">
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Preformer</li>
-            <li>Event Schedule</li>
+        <ul className="flex w-[100%] justify-between">
+            <li className={liStyle}>Home</li>
+            <li className={liStyle}>About</li>
+            <li className={liStyle}>Preformer</li>
+            <li className={liStyle}>Event Schedule</li>
         </ul>
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 import CenterMenu from './CenterMenu'
 
 function Header() {
+    const buttonStyle =
+    "border-[2px] rounded-[10px] border-[#232A4E] px-[25px] py-[7px]";
   return (
-   <div className="header">
-    <img src={require('../img/MuzicLogo.png')} alt="" />
+   <div className="header bg-[#081730] flex items-center justify-between px-[5rem] pt-[2.4rem] text-[0.8rem]">
+   {/* logo */}
+   <img
+        src={require("../img/MuzicLogo.png")}
+        alt=""
+        className="logo  w-[42px] h-[42px]"
+      />
+      {/* side menu */}
     <CenterMenu/>
-    <div className="buttons">
-        <button>Sign up</button>
-        <button>Log in</button>
+    <div className="buttons flex">
+        <button className={`mr-[35px] hover:bg-[#232A4E] ` + buttonStyle}>Sign up</button>
+        <button className={buttonStyle+` bg-[#232A4E]`}>Log in</button>
     </div>
    </div>
   )
